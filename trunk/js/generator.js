23abe73095c800e4ae386f4082eb74edb5048455
simsab@simsab.de <simsab@simsab.de@fa14eee1-31f0-4147-de2a-05293e82cb1a>
2011-09-02 15:55:18 +0000

    - several layout changes - wrapped js functionality in separated methods no stable commit!

--- trunk/js/generator.js
+++ trunk/js/generator.js
@@ -1,51 +1,96 @@
 $(document).ready(function() {
-	$('#generate').click(function() {
-		var object = {};
-		object.type = $('#type').val();
-		object.key = $('#key').val() || undefined;
-		object.title = $('#title').val();
-		object.description = $('#description').val();
-		object.properties = $.evalJSON($('#properties').val());
-		$('#object').val($.toJSON(object));
-		
-		var type = '$' + object.type;
-		var key = object.key ? ':' + object.key : '';
-		var description = object.description ? "\n\n" + object.description : '';
-		var properties = '(' + $('#properties').val().substring(1, $('#properties').val().length - 1) + ')';
-		$('#bizagi').val(type + key + properties + description);
-	});
-	
-	$('#convert').click(function() {
-		var description = $('#bizagi').val();
-		var matches = description.match(/([\s\S]+\s+)?\$(\w+)(:(\w+))?\(([^\v]*)\)(\s+[\s\S]+)?/);
-		var object = {};
-		object.type = matches[2];
-		object.key = matches[4];
-		object.title = $('#title').val();
-		object.description = $.trim(matches[6]);
-		object.properties = $.evalJSON('{' + matches[5] + '}');
-		$('#object').val($.toJSON(object));
-	});
-	
-	$('#test').click(function() {
-		var object = $('#object').val();
-		var objectSequence = {};
-		objectSequence.objects = [];
-		if (object) {
-			objectSequence.objects.push($.evalJSON(object));
-			
-			var host = 'http://localhost';
-			
-			MotivadoPlayer({
-				baseServiceUrl: host + '/motivado-ui/',
-				basePlayerUrl: host + '/player/',
-				baseAssetUrl: host + '/player/assets/',
-				baseVideoUrl: 'http://videos.motivado.de/',
-				localMode: 'true',
-				objectSequence: $.toJSON(objectSequence),
-				debugMode: 'true',
-				element: 'preview'
-			});
-		}
-	});
+	$('#type').change(configure);
+	$('#generate').click(generate);
+	$('#convert').click(convert);
+	$('#test').click(test);
 });
+
+function addItem(item) {
+	var clone = item.children().last().clone();
+	item.append(clone);
+	clone.children('p:last').children('a.remove').click(function() {
+		removeItem(clone);
+	});
+}
+
+function removeItem(item) {
+	if (item.siblings().length > 0) {
+		item.remove();
+	}
+}
+
+function configure() {
+	if (!$('#type').val() || !$('#' + $('#type').val()).html()) {
+		return;
+	}
+	
+	$('#configuration').html($('#' + $('#type').val()).html());
+	
+	$('#configuration a.add').click(function() {
+		addItem($(this).parent().siblings('ul:first'));
+	});
+	$('#configuration a.remove').click(function() {
+		removeItem($(this).parent().parent());
+	});
+}
+
+function generate() {
+	if (!$('#type').val()) {
+		return;
+	}
+	
+	var object = {};
+	object.type = $('#type').val();
+	object.key = $('#key').val() || undefined;
+	object.title = $('#title').val();
+	object.description = $('#description').val();
+	
+	$('#configuration *[name^="properties"]').each(function(index, element) {
+		alert($(element).attr('name') + ' = ' + $(element).val());
+	});
+	
+	$('#object').val($.toJSON(object));
+	
+	/*var type = '$' + object.type;
+	var key = object.key ? ':' + object.key : '';
+	var description = object.description ? "\n\n" + object.description : '';
+	var properties = '(' + $('#properties').val().substring(1, $('#properties').val().length - 1) + ')';
+	$('#modelingName').val(object.title);
+	$('#modelingDescription').val(type + key + properties + description);*/
+}
+
+function convert() {
+	/*var description = $('#modelingDescription').val();
+	var matches = description.match(/([\s\S]+\s+)?\$(\w+)(:(\w+))?\(([^\v]*)\)(\s+[\s\S]+)?/);
+	var object = {};
+	object.type = matches[2];
+	object.key = matches[4];
+	object.title = $('#title').val();
+	object.description = $.trim(matches[6]);
+	object.properties = $.evalJSON('{' + matches[5] + '}');
+	$('#object').val($.toJSON(object));*/
+}
+
+function test() {
+	generate();
+	
+	var object = $('#object').val();
+	var objectSequence = {};
+	
+	objectSequence.objects = [];
+	if (object) {
+		objectSequence.objects.push($.evalJSON(object));
+		var host = 'http://localhost';
+		
+		MotivadoPlayer({
+			baseServiceUrl: host + '/motivado-ui/',
+			basePlayerUrl: host + '/player/',
+			baseAssetUrl: host + '/player/assets/',
+			baseVideoUrl: 'http://videos.motivado.de/',
+			localMode: 'true',
+			objectSequence: $.toJSON(objectSequence),
+			debugMode: 'true',
+			element: 'preview'
+		});
+	}
+}
